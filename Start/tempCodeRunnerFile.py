ae9dfdb9b6652db21855e30117fad00d699ae8cb
Chetan Chauhan <chetanchauhan247@gmail.com>
2026-05-12 18:42:34 +0530

    reverse

--- Start/tempCodeRunnerFile.py
+++ Start/tempCodeRunnerFile.py
@@ -1,11 +1,13 @@
-def nCr(n,r):
-    result=1
-    for i in range(r):
-        result=result*(n-i)
-        result=result//(i+1)
-    return result
-def line(k):
-    for r in range(k):
-        print(nCr(k-1,r))
+def dublicate(arr):
+#     n=len(arr)
+#     i=0
+#     for j in range(1,n):
+#         if(arr[j]!=arr[i]):
+#             arr[i+1]=arr[j]
+#             i+=1
+#     print(arr[:i+1])
+#     return i+1
+
         
-(line(5))
+# arr=[1,1,1,1,2,2,3,3,3,3,3,4]
+# print(dublicate(arr))
